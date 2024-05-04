346998d71c36f355b663027d00d45197a32d1fd0
kitsuyui <kitsuyui@kitsuyui.com>
2024-05-04 14:14:49 +0900

    Implement generate-web-icons script
    This script generates a set of web icons from a single source image.
    
    Generate various web icons by given image file
    Usage: `./icons.sh <image_file>.<ext>`
    Output: `<image_file>.tar.gz`
    Dependencies: ImageMagick
    Note: The input image file should be square.
    The output tarball contains the following files:
    - favicon16.ico
    - favicon32.ico
    - favicon48.ico
    - favicon64.ico
    - apple-touch-icon57.png
    - apple-touch-icon60.png
    - apple-touch-icon72.png
    - apple-touch-icon76.png
    - apple-touch-icon114.png
    - apple-touch-icon120.png
    - apple-touch-icon144.png
    - apple-touch-icon152.png
    - apple-touch-icon180.png
    - favicon16.png
    - favicon32.png
    - favicon48.png
    - favicon96.png
    - favicon192.png

---
+++ tests/test-generate-web-icons.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+PRESERVE_PATH="$PATH"
+source "$PROJECT_ROOT/tests/setup.sh"
+
+# Get example .png icon file
+icon_file="$PROJECT_ROOT/assets/icons/example-icon.png"
+
+assert_tar_contains_target_file() {
+  target_tarball="$1"
+  target_file="$2"
+  if ! tar -ztf "$target_tarball" "$target_file" &> /dev/null; then
+    echo "Failed to generate web icons"
+    echo "Tarball file not found: $target_file"
+    exit 1
+  fi
+}
+
+set +e
+export PATH="$PROJECT_ROOT/bin:$PRESERVE_PATH"
+result=$(generate-web-icons "$icon_file")
+status=$?
+tobe_tarball="./example-icon.tar.gz"
+
+if [ $status -ne 0 ]; then
+  echo "Failed to generate web icons"
+  echo "$result"
+  exit 1
+fi
+
+if [ ! -f "$tobe_tarball" ]; then
+  echo "Failed to generate web icons"
+  echo "Tarball file not found: $tobe_tarball"
+  exit 1
+fi
+
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon.ico"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon16.ico"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon32.ico"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon48.ico"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon64.ico"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon57.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon60.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon72.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon76.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon114.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon120.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon144.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon152.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/apple-touch-icon180.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon16.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon32.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon48.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon96.png"
+assert_tar_contains_target_file "$tobe_tarball" "example-icon/favicon192.png"
+
+set -e
+
+source "$PROJECT_ROOT/tests/teardown.sh"
